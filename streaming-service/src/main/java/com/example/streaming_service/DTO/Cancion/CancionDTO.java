b3fb947465b697780a3e0d1f4a1b581d97280ba8
ralbertor <ralbertor@hotmail.es>
2024-11-26 11:14:19 +0100

    ya funciona
    Antes no funcionaba el genero, pero ya si

--- streaming-service/src/main/java/com/example/streaming_service/DTO/Cancion/CancionDTO.java
+++ streaming-service/src/main/java/com/example/streaming_service/DTO/Cancion/CancionDTO.java
@@ -2,6 +2,8 @@ package com.example.streaming_service.DTO.Cancion;
 
 import java.util.List;
 
+import com.example.streaming_service.entidades.Genero;
+
 import lombok.Data;
 @Data
 public class CancionDTO {
@@ -10,7 +12,7 @@ public class CancionDTO {
     private String urlCancion;
     //private List<CancionArtistaDTO> artistas;
     //private CancionAlbumDTO album;
-    private List<Integer> generosIds;
+    private List<Genero> generos;
 
     
 }
--- streaming-service/src/main/java/com/example/streaming_service/service/CancionService.java
+++ streaming-service/src/main/java/com/example/streaming_service/service/CancionService.java
@@ -12,8 +12,6 @@ import com.example.streaming_service.DTO.Cancion.CancionDTO;
 import com.example.streaming_service.entidades.Cancion;
 import com.example.streaming_service.entidades.Genero;
 import com.example.streaming_service.repositorios.CancionRepository;
-import com.example.streaming_service.repositorios.GeneroRepository;
-
 import jakarta.transaction.Transactional;
 
 @Service
@@ -22,10 +20,7 @@ public class CancionService {
     @Autowired
     private CancionRepository cancionRepo;
 
-    @Autowired
-    private GeneroRepository generoRepo;
-    
-    public Cancion createCancionWithGeneros(CancionDTO cancionDTO){
+    public Cancion createCancionWithGeneros(CancionDTO cancionDTO) {
         //Crear la cancion
         Cancion cancion = new Cancion();
         cancion.setTitulo(cancionDTO.getTitulo());
@@ -34,28 +29,22 @@ public class CancionService {
 
         //Obtener los géneros y asignarlos a la canción
         List<Genero> generos = new ArrayList<>();
-        for (Integer generoid : cancionDTO.getGenerosIds()) {
-            Genero genero = generoRepo.findById(generoid).orElseThrow(() -> 
-            new RuntimeException("genero no encontrado"));
-            generos.add(genero);
-            
-        }
         cancion.setGeneros(generos); //Asignar los géneros a la cancion
-        
+
         //Guardar la canción con géneros asociados
         return cancionRepo.save(cancion);
     }
 
-
-    public Cancion createCancion(Cancion cancion){
-        if(cancion.getTitulo()==null || cancion.getDuracion()==0){
+    public Cancion createCancion(Cancion cancion) {
+        if (cancion.getTitulo() == null || cancion.getDuracion() == 0) {
             throw new IllegalArgumentException("Título y duración obligatoria");
         }
         return cancionRepo.save(cancion);
     }
+
     @Transactional
-    public Cancion updateCancion(int id, Cancion cancion){
-        if(cancionRepo.existsById(id)){
+    public Cancion updateCancion(int id, Cancion cancion) {
+        if (cancionRepo.existsById(id)) {
             Cancion nuevaCancion = new Cancion();
             nuevaCancion.setId(cancion.getId());
             nuevaCancion.setTitulo(cancion.getTitulo());
@@ -64,20 +53,23 @@ public class CancionService {
             nuevaCancion.setAlbum(cancion.getAlbum());
             createCancion(nuevaCancion);
             return nuevaCancion;
+        } else {
+            return null;
         }
-        else return null;
     }
+
     @Transactional
-    public void deleteCancion(int id){
-        if(cancionRepo.existsById(id)){
+    public void deleteCancion(int id) {
+        if (cancionRepo.existsById(id)) {
             cancionRepo.deleteById(id);
             System.out.println("200 canción eliminada correctamente");
+        } else {
+            System.out.println("400, la canción no existe");
         }
-        else System.out.println("400, la canción no existe");
     }
-    
-    public Page<Cancion> buscarCancionPorFiltro(String filtro, Pageable pageable){
+
+    public Page<Cancion> buscarCancionPorFiltro(String filtro, Pageable pageable) {
         return cancionRepo.buscarPorFiltro(filtro, pageable);
     }
-    
+
 }
